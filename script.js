15577aa2aa286f01ae0c02c53921545974de01cf
builderkdev <76799302+builderkdev@users.noreply.github.com>
2023-04-25 09:51:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// create a button element
-const button = document.createElement("button");
-button.innerHTML = "Click me";
 
-// add an event listener to the button
-button.addEventListener("click", function() {
-  alert("Button clicked!");
-});
-
-// add the button to the HTML document
-document.body.appendChild(button);
